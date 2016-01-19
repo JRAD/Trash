4d5d5c5a54db1336492763b0d2aa3727f8eababc
JRAD <jawagner@gmail.com>
2016-01-18 22:28:01 -0800

    Implemented a function that finds the number of pairs that have K difference.

--- src/com/junk/Main.java
+++ src/com/junk/Main.java
@@ -1,8 +1,37 @@
 package com.junk;
 
+import java.io.Console;
+import java.util.HashMap;
+import java.util.Hashtable;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+    }
+
+    public void KDifference()
+    {
+        int [] sample = {1,7,5,9,2,12,3};
+        int k = 2;
+        HashMap results = new HashMap();
+        int count = 0;
+
+        HashMap hashMap = new HashMap();
+        for (int i = 0; i < sample.length; i++)
+        {
+            hashMap.put(i, sample[i]);
+        }
+
+        for (int item: sample )
+        {
+            if(hashMap.containsValue(item + k))
+            {
+                count++;
+            }
+        }
+
+        System.out.print(count);
     }
 }
